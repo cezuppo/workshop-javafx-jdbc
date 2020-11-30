f878eee00c19ac2eb150d177a7540d0e7d3f0f40
cezuppo <czuppo@gmail.com>
2020-11-30 09:09:20 -0300

    Department ComboBox

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -31,6 +31,7 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
+import model.services.DepartmentService;
 import model.services.SellerService;
 
 public class SellerListController implements Initializable, DataChangeListener {
@@ -112,16 +113,24 @@ public class SellerListController implements Initializable, DataChangeListener {
 		  FXMLLoader(getClass().getResource(absoluteName)); Pane pane = loader.load();
 		  
 		  SellerFormController controller = loader.getController();
-		  controller.setSeller(obj); controller.setSellerService(new SellerService());
-		  controller.subscribeDataChangeListener(this); controller.updateFormData();
+		  controller.setSeller(obj); 
+		  controller.setServices(new SellerService(), new DepartmentService());
+		  controller.loadAssociatedObjects();
+		  controller.subscribeDataChangeListener(this); 
+		  controller.updateFormData();
 		  
 		  Stage dialogStage = new Stage();
-		  dialogStage.setTitle("Enter department data"); dialogStage.setScene(new
-		  Scene(pane)); dialogStage.setResizable(false);
+		  dialogStage.setTitle("Enter seller data"); 
+		  dialogStage.setScene(new Scene(pane)); 
+		  dialogStage.setResizable(false);
 		  dialogStage.initOwner(parentStage);
-		  dialogStage.initModality(Modality.WINDOW_MODAL); dialogStage.showAndWait(); }
-		  catch (IOException e) { Alerts.showAlert("IO Exception",
-		  "Error loading view", e.getMessage(), AlertType.ERROR); }
+		  dialogStage.initModality(Modality.WINDOW_MODAL); 
+		  dialogStage.showAndWait(); 
+		  }
+		  catch (IOException e) {
+			  e.printStackTrace();
+			  Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR); 
+		  }
 		 
 	}
 
